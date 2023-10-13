a623f7080363aa2d3f9500b9a4507fbd9ba26a16
PyltsynMaksim <145790861+PyltsynMaksim@users.noreply.github.com>
2023-10-13 19:22:39 +0300

    Update2 discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,21 @@
 import discord
+from discord.ext import commands
 
-from bot_logic import *
-
-
-
-#Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
- #Включаем привелегию на чтение сообщений
 intents.message_content = True
- #Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=discord.Intents.default())
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    elif message.content.startswith('$emodji'):
-        await message.channel.send(gen_emodji())
-    else:
-        await message.channel.send(message.content)
-client.run("MTE1OTg3NzY4NDUwODIzMzc0OQ.Gkl2dr.BlSi3D2LAyHK83q3Z31ZbMagPoEIUHAuxuwd9E")
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTE1OTg3NzY4NDUwODIzMzc0OQ.GPMDFN.dcJmN3--3QN6sFv8vkWEh65GMTpJUzW5fPErbY")
